cb6943b09abcfbad428b4a385ba4e8ac57bf6e79
lsotogar <157931246+lsotogar@users.noreply.github.com>
2024-11-10 01:09:00 +0100

    validaciones de juegos y area

--- validaciones/area.js
+++ validaciones/area.js
@@ -1,8 +1,94 @@
 db.createCollection("area")
 db.mantenimiento.createIndex({"ID": 1}, {unique:1})
 
-VAL = {
+VAL = {bsonType: "object",
+            title: "Areas Validator",
+            required: ["ID", "DESC_CLASIFICACION", "COD_BARRIO", "BARRIO", "COD_DISTRITO", "DISTRITO", "ESTADO", "DIRECCION_AUX", "SISTEMA_COORD", "CODIGO_INTERNO", "CONTRATO_COD", "TOTAL_ELEM", "tipo", "TIPO_VIA", "NOM_VIA", "NUM_VIA", "COD_POSTAL", "FECHA_INSTALACION"],
+            properties: {
+                ID: {
+                    bsonType: "string",
+                    description: "id del área"
+                },
+                DESC_CLASIFICACION: {
+                    bsonType: "string",
+                    enum: ["area de juegos/especial", "area de mayores", "area infantil", "circuito deportivo elemental"],
+                    description: "descripción del tipo de área recreativa"
+                },
+                COD_BARRIO: {
+                    bsonType: "int",
+                    description: "código del barrio al que pertenece el área"
+                },
+                BARRIO: {
+                    bsonType: "string",
+                    description: "barrio al que pertenece el área"
+                },
+                DIRECCION_AUX: {
+                    bsonType: "string",
+                    description: "direccion_aux es un string"
+                },
+                SISTEMA_COORD: {
+                    bsonType: "string",
+                    description: "sistema_coord es un string"
+                },
+                CODIGO_INTERNO: {
+                    bsonType: "string",
+                    description: "codigo_interno es un string"
+                },
+                CONTRATO_COD: {
+                    bsonType: "string",
+                    description: "contrato_cod es un string"
+                },
+                TOTAL_ELEM: {
+                    bsonType: "int",
+                    description: "el total de elementos de un area es un int"
+                },
+                tipo: {
+                    bsonType: "string",
+                    enum: ["infantil", "mayores", "deportivas"],
+                    description: "el tipo de juego tiene que ser uno de esos strings"
+                },
+
+                COD_DISTRITO: {
+                    bsonType: "int",
+                    description: "código del distrito al que pertenece el área"
+                },
+                DISTRITO: {
+                    bsonType: "string",
+                    description: "distrito al que pertenece el área"
+                },
+                ESTADO: {
+                    bsonType: "string",
+                    enum: ["operativo"],
+                    description: "estado del área"
+                },
+                TIPO_VIA: {
+                    bsonType: "string",
+                    description: "tipo del vía donde se encuentra el área"
+                },
+                NOM_VIA: {
+                    bsonType: "string",
+                    description: "nombre de la vía donde se encuentra el área"
+                },
+                NUM_VIA: {
+                    bsonType: "string",
+                    description: "número de la vía donde se encuentra el área"
+                },
+                COD_POSTAL: {
+                    bsonType: ["string", "double"],
+                    description: "código postal de la zona postal en la que se encuentra el área"
+                },
+                FECHA_INSTALACION: {
+                    bsonType: ["date", "string"],
+                    description: "fecha en la que se instaló el área"
+                },
+
+                }
+            }
+
+
+
+
     
-}
+
 
 db.runCommand({ "collMod": "area", "validator": { $jsonSchema: VAL } })
--- validaciones/juegos.js
+++ validaciones/juegos.js
@@ -1,8 +1,101 @@
 db.createCollection("juegos")
 db.mantenimiento.createIndex({"ID": 1}, {unique:1})
 
-VAL = {
-    
-}
+VAL = {bsonType: "object",
+            title: "juegos Validator",
+            required: ["ID", "DESC_CLASIFICACION", "COD_BARRIO", "BARRIO", "COD_DISTRITO", "DISTRITO", "ESTADO",  "TIPO_VIA", "NOM_VIA", "NUM_VIA", "COD_POSTAL", "DIRECCION_AUX", "FECHA_INSTALACION", "CODIGO_INTERNO", "CONTRATO_COD", "MODELO", "tipo_juego", "ACCESIBLE", "INDICADOR_EXPOSICION", "DESGASTE_ACUMULADO", "SISTEMA_COORD"],
+            properties: {
+                ID: {
+                    bsonType: "string",
+                    description: "id del juego"
+                },
+                DESC_CLASIFICACION: {
+                    bsonType: "string",
+                    enum: ["AREAS DE JUEGO/ESPECIAL", "AREAS DE MAYORES", "AREAS INFANTIL", "CIRCUITO DEPORTIVO ELEMENTAL"],
+                    description: "descripción del tipo de juego recreativa"
+                },
+                COD_BARRIO: {
+                    bsonType: "int",
+                    description: "código del barrio al que pertenece el juego"
+                },
+                BARRIO: {
+                    bsonType: "string",
+                    description: "barrio al que pertenece el juego"
+                },
+                COD_DISTRITO: {
+                    bsonType: "int",
+                    description: "código del distrito al que pertenece el juego"
+                },
+                DISTRITO: {
+                    bsonType: "string",
+                    description: "distrito al que pertenece el juego"
+                },
+                ESTADO: {
+                    bsonType: "string",
+                    enum: ["OPERATIVO"],
+                    description: "estado del juego"
+                },
+
+                TIPO_VIA: {
+                    bsonType: "string",
+                    description: "tipo del vía donde se encuentra el juego"
+                },
+                NOM_VIA: {
+                    bsonType: "string",
+                    description: "nombre de la vía donde se encuentra el juego"
+                },
+                NUM_VIA: {
+                    bsonType: "string",
+                    description: "número de la vía donde se encuentra el juego"
+                },
+                COD_POSTAL: {
+                    bsonType: "string",
+                    description: "código postal de la zona postal en la que se encuentra el juego"
+                },
+                DIRECCION_AUX: {
+                    bsonType: "string",
+                    description: "direccion_aux es un stirng"
+                },
+
+                FECHA_INSTALACION: {
+                    bsonType: ["string", "date"],
+                    description: "fecha en la que se instaló el juego"
+                },
+                CODIGO_INTERNO: {
+                    bsonType: "string",
+                    description: "codigo_interno es un string"
+                },
+                CONTRATO_COD: {
+                    bsonType: "string",
+                    description: "cond_contrato es un string"
+                },
+
+                MODELO: {
+                    bsonType: "string",
+                    description: "modelo de juego"
+                },
+                tipo_juego: {
+                    bsonType: "string",
+                    enum: ["deportivas", "infantiles", "mayores"],
+                    description: "tipo de juego"
+                },
+                ACCESIBLE: {
+                    bsonType: "bool",
+                    description: "indica si el juego es accesible"
+                },
+                INDICADOR_EXPOSICION: {
+                    bsonType: "string",
+                    enum: ["alto", "medio", "bajo"],
+                    description: "indicador_exposicion solo puede ser uno de esos strings"
+                },
+                SISTEMA_COORD: {
+                    bsonType: "string",
+                    description: "sistema_coord es un string"
+                },
+                DESGASTE_ACUMULADO: {
+                    bsonType: "int",
+                    description: "desgaste acumulado es un int"
+                }
+            }}
 
 db.runCommand({ "collMod": "juegos", "validator": { $jsonSchema: VAL } })
